280980e9ee99f602820dd17634abe2ac2f7e4bd8
Andrei Cibotariu <andreicibotariu@outlook.com>
2024-04-10 17:51:20 +0300

    added android app

--- components/exceptions/FirebaseUninitializedException.py
+++
@@ -1,4 +0,0 @@
-
-class FirebaseUninitializedException(Exception):
-    def __init__(self):
-        super().__init__("Firebase is not initialized.")
--- utils/datetime_utils.py
+++
@@ -1,6 +0,0 @@
-import datetime
-from tzlocal import get_localzone
-
-
-def get_current_datetime_tz():
-    return datetime.datetime.now(get_localzone())
